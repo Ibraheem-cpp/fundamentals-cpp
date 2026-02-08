73ac72df99d862969e0c507106a3dddef88dd6c1
Ibraheem-cpp <ibraheemtahir84@gmail.com>
2026-02-08 23:07:00 +0500

    Simple Bill Calculator
    Implementation of Bill Calculator for 4 individual persons living in one flat.

---
+++ prac.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    // float current_bill;
+    // float current_units;
+    // float previous_units;
+    // float consumed_units;
+    // int water_bill = 300;
+    // int gas_bill = 300;
+    
+
+    // cout << "Enter Current Bill : ";
+    // cin >> current_bill;
+
+    // cout << "Enter Current Units : ";
+    // cin >> current_units;
+    // cout << "Enter Previous Units : ";
+    // cin >> previous_units;
+
+    // consumed_units = current_units - previous_units;
+    // cout << " Consumed Units : " << consumed_units << endl;
+
+    // float per_unit = current_bill / consumed_units;
+    // cout << "Per Unit Rate = " << per_unit << endl << endl;
+
+    
+
+    //  // ROOM 1
+    //  cout << "    Room 1" << endl;
+    // float room_1_current_unit,room_1_previous_unit,room_1_consumed_unit,room_1_estimate_bill;
+    // cout << "Enter Room 1 Current Units : ";
+    // cin >> room_1_current_unit;
+    // cout << "Enter Room 1 Previous Units : ";
+    // cin >> room_1_previous_unit;
+    // room_1_consumed_unit = room_1_current_unit - room_1_previous_unit;
+    // cout << "Room 1 Consumed Units = " << room_1_consumed_unit << endl;
+    // room_1_estimate_bill = room_1_consumed_unit * per_unit;
+    // cout << "Room 1 Bill : " << room_1_estimate_bill << endl << endl;
+
+
+
+    // // ROOM 2
+    // cout << "       Room 2 " << endl;
+    // float room_2_current_unit,room_2_previous_unit,room_2_consumed_unit,room_2_estimate_bill;
+    // cout << "Enter Room 2 Current Units : ";
+    // cin >> room_2_current_unit;
+    // cout << "Enter Room 2 Previous Units : ";
+    // cin >> room_2_previous_unit;
+    // room_2_consumed_unit = room_2_current_unit - room_2_previous_unit;
+    // cout << "Room 2 Consumed Units = " << room_2_consumed_unit << endl;
+    // room_2_estimate_bill = room_2_consumed_unit * per_unit;
+    // cout << "Room 2 Bill : " << room_2_estimate_bill << endl << endl;
+
+
+
+    // // ROOM 3
+    // cout << "    Room 3 " << endl;
+    // float room_3_current_unit,room_3_previous_unit,room_3_consumed_unit,room_3_estimate_bill;
+    // cout << "Enter Room 3 Current Units : ";
+    // cin >> room_3_current_unit;
+    // cout << "Enter Room 3 Previous Units : ";
+    // cin >> room_3_previous_unit;
+    // room_3_consumed_unit = room_3_current_unit - room_3_previous_unit;
+    // cout << "Room 3 Consumed Units = " << room_3_consumed_unit << endl;
+    // room_3_estimate_bill = room_3_consumed_unit * per_unit;
+    // cout << "Room 3 Bill : " << room_3_estimate_bill << endl << endl;
+
+
+
+    // // ROOM 4
+    // cout << "    Room 4 " << endl;
+    // float room_4_current_unit,room_4_previous_unit,room_4_consumed_unit,room_4_estimate_bill;
+    // cout << "Enter Room 4 Current Units : ";
+    // cin >> room_4_current_unit;
+    // cout << "Enter Room 4 Previous Units : ";
+    // cin >> room_4_previous_unit;
+    // room_4_consumed_unit = room_4_current_unit - room_4_previous_unit;
+    // cout << "Room 4 Consumed Units = " << room_4_consumed_unit << endl;
+    // room_4_estimate_bill = room_4_consumed_unit * per_unit;
+    // cout << "Room 4 Bill : " << room_4_estimate_bill << endl << endl;
+
+
+
+
+    // float estimate_total_bill = room_1_estimate_bill + room_2_estimate_bill + room_3_estimate_bill + room_4_estimate_bill;
+    // cout << "Bill According To Units : " << estimate_total_bill << endl;
+    // cout << "Actual Total Bill : " << current_bill << endl;
+    // float common_bill = current_bill - estimate_total_bill;
+    // cout << "Common Bill = " << common_bill << endl;
+    // float per_head_common = common_bill / 4;
+    // cout << "Common Bill Per head : " << per_head_common << endl;
+
+
+
+    // float room_1_actual_bill,room_2_actual_bill,room_3_actual_bill,room_4_actual_bill;
+    // room_1_actual_bill = room_1_estimate_bill + per_head_common + water_bill + gas_bill;
+    // room_2_actual_bill = room_2_estimate_bill + per_head_common + water_bill + gas_bill;
+    // room_3_actual_bill = room_3_estimate_bill + per_head_common + water_bill + gas_bill;
+    // room_4_actual_bill = room_4_estimate_bill + per_head_common + water_bill + gas_bill;
+
+
+    // cout << endl << endl << endl;
+
+    //  cout << "        ROOM 1  " << endl;
+    // cout << "Current Units : " << room_1_current_unit << endl;
+    // cout << "Previous Units : " << room_1_previous_unit << endl;
+    // cout << "Consumed Units : " << room_1_consumed_unit << endl;
+    // cout << "Common Bill : " << per_head_common << endl;
+    // cout << "Water Bill : " << water_bill << endl;
+    // cout << "Gas Bill : " << gas_bill << endl;
+    // cout << "----------------------------" << endl;
+    // cout << "TOTAL BILL = " << room_1_actual_bill;
+    // cout << endl << endl << endl;
+
+
+    //  cout << "        ROOM 2  " << endl;
+    // cout << "Current Units : " << room_2_current_unit << endl;
+    // cout << "Previous Units : " << room_2_previous_unit << endl;
+    // cout << "Consumed Units : " << room_2_consumed_unit << endl;
+    // cout << "Common Bill : " << per_head_common << endl;
+    // cout << "Water Bill : " << water_bill << endl;
+    // cout << "Gas Bill : " << gas_bill << endl;
+    // cout << "----------------------------" << endl;
+    // cout << "TOTAL BILL = " << room_2_actual_bill;
+    // cout << endl << endl << endl;
+
+
+    //  cout << "        ROOM 3  " << endl;
+    // cout << "Current Units : " << room_3_current_unit << endl;
+    // cout << "Previous Units : " << room_3_previous_unit << endl;
+    // cout << "Consumed Units : " << room_3_consumed_unit << endl;
+    // cout << "Common Bill : " << per_head_common << endl;
+    // cout << "Water Bill : " << water_bill << endl;
+    // cout << "Gas Bill : " << gas_bill << endl;
+    // cout << "----------------------------" << endl;
+    // cout << "TOTAL BILL = " << room_3_actual_bill;
+    // cout << endl << endl << endl;
+
+
+    //  cout << "        ROOM 4  " << endl;
+    // cout << "Current Units : " << room_4_current_unit << endl;
+    // cout << "Previous Units : " << room_4_previous_unit << endl;
+    // cout << "Consumed Units : " << room_4_consumed_unit << endl;
+    // cout << "Common Bill : " << per_head_common << endl;
+    // cout << "Water Bill : " << water_bill << endl;
+    // cout << "Gas Bill : " << gas_bill << endl;
+    // cout << "----------------------------" << endl;
+    // cout << "TOTAL BILL = " << room_4_actual_bill;
+    // cout << endl << endl << endl;
+
+    return 0;
+}
